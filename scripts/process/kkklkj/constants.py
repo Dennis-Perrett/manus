b9cbfb2f42390be6690a1e113da7801a4cec4564
Dennis Perrett <perrettde@cgpoolsand1900.cs.uni-tuebingen.de>
2024-06-14 10:07:25 +0200

    removed local blender_wormholes

--- scripts/process/kkklkj/constants.py
+++
@@ -1,7 +0,0 @@
-SHADER_NODE_TYPE = {
-    'diffuse': "ShaderNodeBsdfDiffuse",
-    'emission': "ShaderNodeEmission",
-    'image_texture': "ShaderNodeTexImage",
-    'wireframe': "ShaderNodeWireframe",
-    'mix_shader': "ShaderNodeMixShader"
-}
